ad126b22191b3abf2521ae0d909a2bc175fea619
seojun2836 <117098898+seojun2836@users.noreply.github.com>
2025-12-02 00:36:50 +0900

    Update QuestionService.java

--- src/main/java/com/mysite/sbb/question/service/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/service/QuestionService.java
@@ -4,8 +4,7 @@ import com.mysite.sbb.member.entity.Member;
 import com.mysite.sbb.question.dto.QuestionDto;
 import com.mysite.sbb.question.entity.Question;
 import com.mysite.sbb.question.repository.QuestionRepository;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,43 +16,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    // 페이지 기능 추가
-    public Page<Question> getList(int page) {
-        List<Sort.Order> sorts = new ArrayList<Sort.Order>(); // 여러 정렬 기준을 위한 리스트
-        sorts.add(Sort.Order.desc("created")); // 생성일 기준 내림차순 정렬
-        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 페이지당 10개씩 조회 + 정렬 기능
-        Page<Question> questionList = questionRepository.findAll(pageable);
-        return questionList;
+    public Page<Question> getQuestionList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("created"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return questionRepository.findAll(pageable);
     }
 
     public Question getQuestion(Long id) {
         Question question = questionRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("id에 해당하는 질문이 없습니다 : " + id));
+                .orElseThrow(() -> new EntityNotFoundException("해당질문 X"));
         return question;
+
+//        Optional<Question> q = questionRepository.findById(id);
+//        if(q.isPresent()){
+//            Question question = q.get();
+//            System.out.println(question);
+//        } else {
+//            throw new EntityNotFoundException("해당 질문 X");
+//        }
     }
 
     public void create(QuestionDto questionDto, Member member) {
         Question question = Question.builder()
                 .subject(questionDto.getSubject())
                 .content(questionDto.getContent())
-                .author(member) // 질문 작성자 설정(추가)
+                .author(member)
                 .build();
         questionRepository.save(question);
-    }
 
-    public void modify(Question question, String subject, String content) {
-        question.setSubject(subject);
-        question.setContent(content);
-        questionRepository.save(question);
-    }
-
-    public void delete(Question question) {
-        questionRepository.delete(question);
     }
 }
