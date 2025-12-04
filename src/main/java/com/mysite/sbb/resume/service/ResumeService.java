ef15c17bce11e2ce3200365ae87c86020afb05ef
taeho0320 <kangtaeho0320@gmail.com>
2025-12-04 14:27:11 +0900

    Add files via upload

--- src/main/java/com/mysite/sbb/resume/service/ResumeService.java
+++ src/main/java/com/mysite/sbb/resume/service/ResumeService.java
@@ -4,10 +4,8 @@ import com.mysite.sbb.resume.dto.ResumeDto;
 import com.mysite.sbb.resume.entity.Resume;
 import com.mysite.sbb.resume.repository.ResumeRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,7 +17,6 @@ public class ResumeService {
 
     // 이력서 목록 조회
     public List<Resume> getList() {
-        // DB에 저장된 모든 이력서를 조회하여 반환합니다.
         return this.resumeRepository.findAll();
     }
 
@@ -29,13 +26,12 @@ public class ResumeService {
         if (resume.isPresent()) {
             return resume.get();
         } else {
-            throw new RuntimeException("Resume not found"); // 해당 ID의 이력서가 없을 경우 예외 발생
+            throw new RuntimeException("Resume not found");
         }
     }
 
     // 이력서 생성 및 저장
     public void create(ResumeDto resumeDto) {
-        // DTO를 Entity로 변환하여 저장합니다.
         Resume resume = Resume.builder()
                 .name(resumeDto.getName())
                 .phone(resumeDto.getPhone())
@@ -44,10 +40,23 @@ public class ResumeService {
                 .careerDetails(resumeDto.getCareerDetails())
                 .selfIntroduction(resumeDto.getSelfIntroduction())
                 .build();
+        this.resumeRepository.save(resume);
+    }
+
+    // [추가된 기능] 이력서 수정 메서드
+    public void modify(Resume resume, ResumeDto resumeDto) {
+        resume.setName(resumeDto.getName());
+        resume.setPhone(resumeDto.getPhone());
+        resume.setEmail(resumeDto.getEmail());
+        resume.setTechStack(resumeDto.getTechStack());
+        resume.setCareerDetails(resumeDto.getCareerDetails());
+        resume.setSelfIntroduction(resumeDto.getSelfIntroduction());
 
         this.resumeRepository.save(resume);
     }
 
-
-
+    // [추가된 기능] 이력서 삭제 메서드
+    public void delete(Resume resume) {
+        this.resumeRepository.delete(resume);
+    }
 }
